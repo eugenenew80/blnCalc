4b40d38f870dc7699d755e69b35233841d924e96
Eugene B <eugenenew80@gmail.com>
2018-05-02 21:46:49 +0600

    Added ignoring whitespaces in xml formula

--- src/main/java/calc/App.java
+++ src/main/java/calc/App.java
@@ -15,6 +15,7 @@ import org.w3c.dom.Element;
 import javax.annotation.PostConstruct;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
@@ -26,15 +27,14 @@ import java.io.File;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.stream.Collectors.*;
+
 @EntityScan(
     basePackageClasses = { App.class, Jsr310JpaConverters.class }
 )
 @SpringBootApplication
 public class App  {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
-    private static final String XSL_FILE = "files/report.xsl";
-    private static final String INPUT_FILE = "files/doc.xml";
-    private static final String OUTPUT_FILE = "files/excel.xml";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -46,130 +46,155 @@ public class App  {
     }
 
     public void buildReport() throws Exception {
-        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
         Report report = reportRepo.findOne(1l);
-        Document doc = docBuilder.newDocument();
+        Document doc = createReportElement(report);
+
+        StreamSource xslCode = new StreamSource(new File("files/report.xsl"));
+        StreamResult output = new StreamResult(new File("files/excel.xml"));
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer trans = tf.newTransformer(xslCode);
+        trans.transform(new DOMSource(doc), output);
+    }
+
+    private Document createReportElement(Report report) throws Exception {
+        Document doc = createDocument();
+
         Element rootElement = doc.createElement("report");
         rootElement.setAttribute("type", report.getReportType());
-
         doc.appendChild(rootElement);
+        createSheetsElement(doc, rootElement, report.getSheets());
 
-        List<ReportSheet> sheets = report.getSheets();
-        for (ReportSheet sheet : sheets) {
+        return doc;
+    }
 
-            Element sheetElement = doc.createElement("sheet");
-            sheetElement.setAttribute("name", sheet.getName());
-            rootElement.appendChild(sheetElement);
+    private Document createDocument() throws Exception {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        return docBuilder.newDocument();
+    }
 
-            sheet.getColumns()
-                .stream()
-                .sorted(Comparator.comparing(SheetColumn::getOrderNum))
-                .forEach(column -> {
-                    Element columnElement = doc.createElement("column");
-                    columnElement.setAttribute("width", column.getWidth().toString());
-                    sheetElement.appendChild(columnElement);
-                });
+    private void createSheetsElement(Document doc, Element parentElement, List<ReportSheet> sheets) {
+        sheets.stream()
+            .forEach(sheet -> {
+                Element sheetElement = doc.createElement("sheet");
+                sheetElement.setAttribute("name", sheet.getName());
+                parentElement.appendChild(sheetElement);
+                sheet.getColumns()
+                    .stream()
+                    .sorted(Comparator.comparing(SheetColumn::getOrderNum))
+                    .forEach(column -> {
+                        Element columnElement = doc.createElement("column");
+                        columnElement.setAttribute("width", column.getWidth().toString());
+                        sheetElement.appendChild(columnElement);
+                    });
 
-            Element headElement = doc.createElement("head");
-            sheetElement.appendChild(headElement);
+                sheetElement.appendChild(createHeaderElement(doc));
 
-            Element repNameElement = doc.createElement("name");
-            repNameElement.appendChild(doc.createTextNode("АКТ"));
-            headElement.appendChild(repNameElement);
+                createTablesElement(doc, sheetElement, sheet.getTables());
+            });
+    }
 
-            Element repPeriodElement = doc.createElement("period");
-            repPeriodElement.setAttribute("start-date", "2018-03-01");
-            repPeriodElement.setAttribute("end-date", "2018-03-31");
-            headElement.appendChild(repPeriodElement);
-
-            Element repEnergyObjectElement = doc.createElement("energy-object");
-            repEnergyObjectElement.setAttribute("type", "subst");
-            repEnergyObjectElement.setAttribute("name", "ПС 500 кВ Шымкент");
-            headElement.appendChild(repEnergyObjectElement);
-
-            List<ReportTable> tables = sheet.getTables();
-            for (ReportTable table : tables) {
-                Element tableElement = doc.createElement("table");
-                tableElement.setAttribute("name", table.getName());
-                sheetElement.appendChild(tableElement);
-
-                if (table.getHasHeader()) {
-                    Element tableHeadElement = doc.createElement("head");
-                    table.getAttrs()
-                        .stream()
-                        .sorted(Comparator.comparing(ReportAttr::getOrderNum))
-                        .forEach(attr -> {
-                            Element tableColumnElement = doc.createElement("column");
-                            tableColumnElement.setAttribute("type", attr.getAttrType().toString().toLowerCase());
-                            tableColumnElement.setAttribute("attr", attr.getName());
-                            tableColumnElement.setAttribute("name", attr.getDescription());
-                            tableHeadElement.appendChild(tableColumnElement);
-                        });
-                    tableElement.appendChild(tableHeadElement);
-                }
-
-
+    private void createTablesElement(Document doc, Element parentElement, List<ReportTable> tables) {
+        tables.stream()
+            .sorted(Comparator.comparing(ReportTable::getId))
+            .forEach(table -> {
+                Element tableElement = createTableElement(doc, table);
+                parentElement.appendChild(tableElement);
                 Element bodyElement = doc.createElement("body");
                 tableElement.appendChild(bodyElement);
 
-                table.getDivisions()
+                createDivisionsElement(doc, bodyElement, table.getDivisions());
+            });
+    }
+
+    private void createDivisionsElement(Document doc, Element parentElement, List<ReportDivision> divisions) {
+        divisions.stream()
+            .sorted(Comparator.comparing(ReportDivision::getOrderNum))
+            .forEach(division -> {
+                Element divisionElement = createDivisionElement(doc, division);
+                parentElement.appendChild(divisionElement);
+
+                createSectionsElement(doc, divisionElement, division.getSections());
+
+                List<ReportRow> rows = division.getRows()
                     .stream()
-                    .sorted(Comparator.comparing(ReportDivision::getOrderNum))
-                    .forEach(division -> {
-                        Element divisionElement = doc.createElement("division");
-                        divisionElement.setAttribute("name", division.getName());
-                        divisionElement.setAttribute("is-total", division.getHasTotal().toString().toLowerCase());
-                        divisionElement.setAttribute("is-title", division.getHasTitle().toString().toLowerCase());
-                        bodyElement.appendChild(divisionElement);
+                    .filter(t -> t.getSection() == null)
+                    .collect(toList());
 
-                        division.getSections()
-                            .stream()
-                            .sorted(Comparator.comparing(ReportSection::getOrderNum))
-                            .forEach(section -> {
-                                Element sectionElement = doc.createElement("section");
-                                sectionElement.setAttribute("name", section.getName());
-                                sectionElement.setAttribute("is-total", section.getHasTotal().toString().toLowerCase());
-                                sectionElement.setAttribute("is-title", section.getHasTitle().toString().toLowerCase());
-                                divisionElement.appendChild(sectionElement);
+                createRowsElement(doc, divisionElement, rows);
+            });
+    }
 
-                                section.getRows()
-                                    .stream()
-                                    .sorted(Comparator.comparing(ReportRow::getOrderNum))
-                                    .forEach(row -> {
-                                        Element rowElement = createRowElement(doc, row);
-                                        sectionElement.appendChild(rowElement);
-                                });
-                        });
+    private void createSectionsElement(Document doc, Element parentElement, List<ReportSection> sections) {
+        sections.stream()
+            .sorted(Comparator.comparing(ReportSection::getOrderNum))
+            .forEach(section -> {
+                Element sectionElement = createSectionElement(doc, section);
+                parentElement.appendChild(sectionElement);
+                createRowsElement(doc, sectionElement, section.getRows());
+            });
+    }
 
-                        division.getRows()
-                            .stream()
-                            .filter(t -> t.getSection()==null)
-                            .sorted(Comparator.comparing(ReportRow::getOrderNum))
-                            .forEach(row -> {
-                                Element rowElement = createRowElement(doc, row);
-                                divisionElement.appendChild(rowElement);
-                            });
-                    });
-            }
+    private void createRowsElement(Document doc, Element parentElement, List<ReportRow> rows) {
+        rows.stream()
+            .sorted(Comparator.comparing(ReportRow::getOrderNum))
+            .forEach(row -> {
+                Element rowElement = createRowElement(doc, row);
+                parentElement.appendChild(rowElement);
+            });
+    }
+
+    private Element createSectionElement(Document doc, ReportSection section) {
+        Element sectionElement = doc.createElement("section");
+        sectionElement.setAttribute("name", section.getName());
+        sectionElement.setAttribute("is-total", section.getHasTotal().toString().toLowerCase());
+        sectionElement.setAttribute("is-title", section.getHasTitle().toString().toLowerCase());
+        return sectionElement;
+    }
+
+    private Element createDivisionElement(Document doc, ReportDivision division) {
+        Element divisionElement = doc.createElement("division");
+        divisionElement.setAttribute("name", division.getName());
+        divisionElement.setAttribute("is-total", division.getHasTotal().toString().toLowerCase());
+        divisionElement.setAttribute("is-title", division.getHasTitle().toString().toLowerCase());
+        return divisionElement;
+    }
+
+    private Element createTableElement(Document doc, ReportTable table) {
+        Element tableElement = doc.createElement("table");
+        tableElement.setAttribute("name", table.getName());
+
+        if (table.getHasHeader()) {
+            Element tableHeadElement = doc.createElement("head");
+            table.getAttrs()
+                .stream()
+                .sorted(Comparator.comparing(ReportAttr::getOrderNum))
+                .forEach(attr -> {
+                    Element tableColumnElement = doc.createElement("column");
+                    tableColumnElement.setAttribute("type", attr.getAttrType().toString().toLowerCase());
+                    tableColumnElement.setAttribute("attr", attr.getName());
+                    tableColumnElement.setAttribute("name", attr.getDescription());
+                    tableHeadElement.appendChild(tableColumnElement);
+                });
+            tableElement.appendChild(tableHeadElement);
         }
+        return tableElement;
+    }
 
-        TransformerFactory tranFactory = TransformerFactory.newInstance();
-        Transformer aTransformer = tranFactory.newTransformer();
-        Source src = new DOMSource(doc);
-        Result dest = new StreamResult(new File(INPUT_FILE));
-        aTransformer.transform(src, dest);
-
-
-        StreamSource xslCode = new StreamSource(new File(XSL_FILE));
-        StreamSource input = new StreamSource(new File(INPUT_FILE));
-        StreamResult output = new StreamResult(new File(OUTPUT_FILE));
-
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer trans = tf.newTransformer(xslCode);
-
-        trans.transform(input, output);
+    private Element createHeaderElement(Document doc) {
+        Element headElement = doc.createElement("head");
+        Element repNameElement = doc.createElement("name");
+        repNameElement.appendChild(doc.createTextNode("АКТ"));
+        headElement.appendChild(repNameElement);
+        Element repPeriodElement = doc.createElement("period");
+        repPeriodElement.setAttribute("start-date", "2018-03-01");
+        repPeriodElement.setAttribute("end-date", "2018-03-31");
+        headElement.appendChild(repPeriodElement);
+        Element repEnergyObjectElement = doc.createElement("energy-object");
+        repEnergyObjectElement.setAttribute("type", "subst");
+        repEnergyObjectElement.setAttribute("name", "ПС 500 кВ Шымкент");
+        headElement.appendChild(repEnergyObjectElement);
+        return headElement;
     }
 
     private Element createRowElement(Document doc, ReportRow row) {
