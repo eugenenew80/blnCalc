91726dbc31d587006eefe3b0a05bc663219362b2
eugenenew80 <eugenenew80@gmail.com>
2018-08-20 17:58:11 +0600

    -

--- src/main/java/calc/formula/service/impl/CalcServiceImpl.java
+++ src/main/java/calc/formula/service/impl/CalcServiceImpl.java
@@ -12,16 +12,9 @@ import calc.formula.service.ExpressionService;
 import calc.repo.calc.FormulaRepo;
 import calc.repo.calc.MeteringPointRepo;
 import calc.repo.calc.ParameterRepo;
-import calc.repo.calc.SourceTypeRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -52,76 +45,6 @@ public class CalcServiceImpl implements CalcService {
         }
 
         return result;
-    }
-
-
-    public List<CalcResult> calc(CalcContext context) throws Exception {
-        Map<String, DoubleExpression> expressionMap = new HashMap<>();
-        Map<String, Formula>  formulaMap = new HashMap<>();
-
-        for (Formula formula : formulaRepo.findAllByMeteringPointOrgId(context.getOrgId())) {
-            if (formula.getStartDate() !=null && context.getEndDate().atStartOfDay().isBefore(formula.getStartDate()))
-                continue;
-
-            if (formula.getEndDate()!=null && context.getStartDate().atStartOfDay().isAfter(formula.getEndDate()))
-                continue;
-
-            if (formula.getMeteringPoint().getMeteringPointTypeId()!=2)
-                continue;
-
-            DoubleExpression expr = expressionService.parse(formula, context);
-
-            expressionMap.putIfAbsent(formula.getMeteringPoint().getCode(), expr);
-            formulaMap.putIfAbsent(formula.getMeteringPoint().getCode(), formula);
-        }
-
-        List<String> mpCodes = expressionService.sort(expressionMap);
-        context.setValues(new ArrayList<>());
-        context.setTrace(new HashMap<>());
-        for (String code : mpCodes) {
-            Formula formula = formulaMap.get(code);
-            MeteringPoint meteringPoint = formula.getMeteringPoint();
-
-            for (MeteringPointParameter fp : meteringPoint.getParameters()) {
-                Parameter parameter = fp.getParameter();
-                Unit unit = parameter.getUnit();
-                DoubleExpression expression = expressionService.parse(formula, parameter.getCode(), context);
-
-                if (parameter.getIsPt()) {
-                    Double[] results = expression.doubleValues();
-                    LocalDateTime meteringDate = context.getStartDate().atStartOfDay();
-                    for (int i = 0; i < results.length; i++) {
-                        CalcResult result = new CalcResult();
-                        result.setMeteringDate(meteringDate.plusHours(i));
-                        result.setDoubleVal(results[i]);
-                        result.setInterval(3600l);
-                        result.setMeteringPointId(meteringPoint.getId());
-                        result.setParamId(parameter.getId());
-                        result.setUnitId(unit.getId());
-                        result.setParamType("PT");
-                        context.getValues().add(result);
-                    }
-                }
-
-                if (parameter.getIsAt()) {
-                    Double[] results = new Double[]{expression.doubleValue()};
-                    LocalDateTime meteringDate = context.getEndDate().atStartOfDay().plusDays(1);
-                    for (int i = 0; i < results.length; i++) {
-                        CalcResult result = new CalcResult();
-                        result.setMeteringDate(meteringDate.plusHours(i));
-                        result.setDoubleVal(results[i]);
-                        result.setInterval(null);
-                        result.setMeteringPointId(meteringPoint.getId());
-                        result.setParamId(parameter.getId());
-                        result.setUnitId(unit.getId());
-                        result.setParamType("AT");
-                        context.getValues().add(result);
-                    }
-                }
-            }
-        }
-
-        return context.getValues();
     }
 
     public String formulaToString(Formula formula) {
