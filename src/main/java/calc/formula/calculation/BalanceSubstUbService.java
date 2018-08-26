78a756ccc7d87074d53cdf99f457d35921b22fae
eugenenew80 <eugenenew80@gmail.com>
2018-08-26 21:51:18 +0600

    -

--- src/main/java/calc/formula/calculation/BalanceSubstUbService.java
+++ src/main/java/calc/formula/calculation/BalanceSubstUbService.java
@@ -7,6 +7,7 @@ import calc.formula.expression.impl.WorkingHoursExpression;
 import calc.formula.service.WorkingHoursService;
 import calc.repo.calc.BalanceSubstResultHeaderRepo;
 import calc.repo.calc.BalanceSubstResultMrLineRepo;
+import calc.repo.calc.BalanceSubstResultUbLineRepo;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,7 @@ public class BalanceSubstUbService {
     private static final Logger logger = LoggerFactory.getLogger(BalanceSubstUbService.class);
     private final BalanceSubstResultHeaderRepo balanceSubstResultHeaderRepo;
     private final BalanceSubstResultMrLineRepo balanceSubstResultMrLineRepo;
+    private final BalanceSubstResultUbLineRepo balanceSubstResultUbLineRepo;
     private final WorkingHoursService workingHoursService;
     private static final String docCode = "UNBALANCE";
 
@@ -39,7 +41,7 @@ public class BalanceSubstUbService {
                 .orgId(header.getOrganization().getId())
                 .energyObjectType("SUBSTATION")
                 .energyObjectId(header.getSubstation().getId())
-                .docCode("UNBALANCE")
+                .docCode(docCode)
                 .docId(header.getId())
                 .trace(new HashMap<>())
                 .values(new HashMap<>())
@@ -47,7 +49,7 @@ public class BalanceSubstUbService {
 
             List<BalanceSubstResultMrLine> mrLines = balanceSubstResultMrLineRepo.findAllByHeaderId(header.getId());
 
-            Double wsum1 = header.getHeader().getUbLines().stream()
+            Double wTotal1 = header.getHeader().getUbLines().stream()
                 .filter(t -> t.getIsSection1())
                 .flatMap(t -> mrLines.stream().filter(l -> l.getMeteringPoint().equals(t.getMeteringPoint())))
                 .filter(t -> t.getParam().getCode().equals("A+") || t.getParam().getCode().equals("A-"))
@@ -56,7 +58,7 @@ public class BalanceSubstUbService {
                 .reduce((t1, t2) -> t1 + t2)
                 .orElse(0d);
 
-            Double wsum2 = header.getHeader().getUbLines().stream()
+            Double wTotal2 = header.getHeader().getUbLines().stream()
                 .filter(t -> t.getIsSection2())
                 .flatMap(t -> mrLines.stream().filter(l -> l.getMeteringPoint().equals(t.getMeteringPoint())))
                 .filter(t -> t.getParam().getCode().equals("A+") || t.getParam().getCode().equals("A-"))
@@ -92,7 +94,23 @@ public class BalanceSubstUbService {
                     for (MeterHistory meterHistory : meterHistories) {
                         BalanceSubstResultUbLine line = new BalanceSubstResultUbLine();
 
+                        TtType ttType = meterHistory.getTtType();
+                        TtType tnType = meterHistory.getTnType();
+                        EemType eemType = meterHistory.getMeter().getEemType();
+
+                        String paramCode = direction.equals("1") ? "A+" : "A-";
+                        Double w = mrLines.stream()
+                            .filter(t -> !t.getIsIgnore())
+                            .filter(t -> t.getMeteringPoint().equals(ubLine.getMeteringPoint()))
+                            .filter(t -> t.getParam().getCode().equals(paramCode))
+                            .filter(t -> t.getMeterHistory() != null)
+                            .filter(t -> t.getMeterHistory().equals(meterHistory))
+                            .map(t -> Optional.ofNullable(t.getVal()).orElse(0d) + Optional.ofNullable(t.getUnderCountVal()).orElse(0d))
+                            .reduce((t1, t2) -> t1 + t2)
+                            .get();
+
                         Double wa = mrLines.stream()
+                            .filter(t -> !t.getIsIgnore())
                             .filter(t -> t.getMeteringPoint().equals(ubLine.getMeteringPoint()))
                             .filter(t -> t.getParam().getCode().equals("A+") || t.getParam().getCode().equals("A-"))
                             .filter(t -> t.getMeterHistory() != null)
@@ -102,6 +120,7 @@ public class BalanceSubstUbService {
                             .get();
 
                         Double wr = mrLines.stream()
+                            .filter(t -> !t.getIsIgnore())
                             .filter(t -> t.getMeteringPoint().equals(ubLine.getMeteringPoint()))
                             .filter(t -> t.getParam().getCode().equals("R+") || t.getParam().getCode().equals("R-"))
                             .filter(t -> t.getMeterHistory() != null)
@@ -111,15 +130,11 @@ public class BalanceSubstUbService {
                             .get();
 
                         Double i1avgVal = Math.sqrt(Math.pow(wa, 2) + Math.pow(wr, 2)) / (Math.sqrt(3) * ratedVoltage);
-
-                        TtType ttType = meterHistory.getTtType();
-                        TtType tnType = meterHistory.getTnType();
-                        EemType eemType = meterHistory.getMeter().getEemType();
-
                         Double i1avgProc = i1avgVal / ttType.getRatedCurrent1();
+                        Double ttAcProc = ttType.getAccuracyClass().getValue();
 
                         Double bttProc;
-                        if (i1avgProc > 100) bttProc = ttType.getAccuracyClass().getValue();
+                        if (i1avgProc > 100) bttProc = ttAcProc;
                         else if (i1avgProc >= 20) bttProc = 0.8125 - 0.003125 * i1avgProc;
                         else if (i1avgProc >= 5) bttProc = 1.75 - 0.05 * i1avgProc;
                         else bttProc = 1.5;
@@ -131,35 +146,74 @@ public class BalanceSubstUbService {
                         Double bProc = Math.pow(biProc, 2) + Math.pow(buProc, 2) + Math.pow(blProc, 2) + Math.pow(bsoProc, 2);
 
                         Double dol = 0d;
-                        if (ubLine.getIsSection1()) dol = wa / wsum1;
-                        if (ubLine.getIsSection2()) dol = wa / wsum2;
+                        if (direction.equals("1")) dol = wa / wTotal1;
+                        if (direction.equals("2")) dol = wa / wTotal2;
 
                         Double b2dol2 = Math.pow(bProc, 2) * Math.pow(dol, 2);
 
-                        line.setB2dol2(b2dol2);
-                        line.setBiProc(biProc);
-                        line.setBlProc(blProc);
-                        line.setBProc(bProc);
-                        line.setBsoProc(bsoProc);
+                        line.setHeader(header);
+                        line.setMeteringPoint(ubLine.getMeteringPoint());
+                        line.setDirection(direction);
+                        line.setW(w);
+                        line.setWa(wa);
+                        line.setWr(wr);
+                        line.setTtStar(null);
+                        line.setTtacProc(ttAcProc);
+                        line.setI1Nom(ttType.getRatedCurrent1());
+                        line.setTRab(workHours);
+                        line.setUavg(ratedVoltage);
+                        line.setI1avgVal(i1avgVal);
+                        line.setI1avgProc(i1avgProc);
                         line.setBttFactor(null);
                         line.setBttProc(bttProc);
+                        line.setBiProc(biProc);
                         line.setBuProc(buProc);
-                        line.setDirection(direction);
+                        line.setBlProc(blProc);
+                        line.setBsoProc(bsoProc);
+                        line.setBProc(bProc);
                         line.setDol(dol);
-                        line.setHeader(header);
-                        line.setI1avgProc(i1avgProc);
-                        line.setI1avgVal(i1avgVal);
-                        line.setI1Nom(ttType.getRatedCurrent1());
-                        line.setMeteringPoint(ubLine.getMeteringPoint());
-                        line.setTRab(workHours);
-                        line.setTtacProc(ttType.getAccuracyClass().getValue());
-                        line.setTtStar(null);
-
+                        line.setB2dol2(b2dol2);
                         lines.add(line);
                     }
                 }
             }
 
+            Double r1SumWa = lines.stream()
+                .filter(t -> t.getDirection().equals("1"))
+                .filter(t -> t.getWa() != null)
+                .map(t -> t.getWa())
+                .reduce((t1, t2) -> t1 + t2)
+                .orElse(0d);
+
+            Double r1SumB2D2 = lines.stream()
+                .filter(t -> t.getDirection().equals("1"))
+                .filter(t -> t.getB2dol2() != null)
+                .map(t -> t.getB2dol2())
+                .reduce((t1, t2) -> t1 + t2)
+                .orElse(0d);
+
+            Double r2SumWa = lines.stream()
+                .filter(t -> t.getDirection().equals("2"))
+                .filter(t -> t.getWa() != null)
+                .map(t -> t.getWa())
+                .reduce((t1, t2) -> t1 + t2)
+                .orElse(0d);
+
+            Double r2SumB2D2 = lines.stream()
+                .filter(t -> t.getDirection().equals("2"))
+                .filter(t -> t.getB2dol2() != null)
+                .map(t -> t.getB2dol2())
+                .reduce((t1, t2) -> t1 + t2)
+                .orElse(0d);
+
+            Double nbdProc = Math.pow(r1SumB2D2 + r2SumB2D2, 2) * 100d;
+            Double nbdVal = nbdProc * r1SumWa / 100d;
+
+            header.setNbdProc(nbdProc);
+            header.setNbdVal(nbdVal);
+
+            balanceSubstResultUbLineRepo.save(lines);
+            balanceSubstResultHeaderRepo.save(header);
             updateStatus(header, BatchStatusEnum.C);
             logger.info("Unbalance for header " + header.getId() + " completed");
         }
