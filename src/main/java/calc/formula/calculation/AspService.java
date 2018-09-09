c2cc1f50ff7a5b9c28eb5dc3d5e837af565a5c27
eugenenew80 <eugenenew80@gmail.com>
2018-09-09 14:48:44 +0600

    -

--- src/main/java/calc/formula/calculation/AspService.java
+++ src/main/java/calc/formula/calculation/AspService.java
@@ -67,107 +67,10 @@ public class AspService {
                 .values(new HashMap<>())
                 .build();
 
-
-            List<AspResultLine> resultLines = new ArrayList<>();
-            for (AspLine aspLine : header.getHeader().getLines()) {
-                if (aspLine.getTreatmentType() != TreatmentTypeEnum.INFO)
-                    continue;;
-
-                AspResultLine line = new AspResultLine();
-                line.setHeader(header);
-                line.setLineNum(aspLine.getLineNum());
-                line.setTreatmentType(aspLine.getTreatmentType());
-                resultLines.add(line);
-                saveLines(resultLines);
-            }
-
-            resultLines = new ArrayList<>();
-            for (AspLine aspLine : header.getHeader().getLines()) {
-                if (aspLine.getTreatmentType() != TreatmentTypeEnum.IN)
-                    continue;
-
-                List<MeteringReading> meteringReadings = meteringReadingService.calc(aspLine.getMeteringPoint(), context)
-                    .stream()
-                    .filter(t -> t.getParam().equals(aspLine.getParam()))
-                    .collect(Collectors.toList());
-
-                for (MeteringReading t : meteringReadings) {
-                    AspResultLine line = new AspResultLine();
-                    line.setHeader(header);
-                    line.setLineNum(aspLine.getLineNum());
-                    line.setMeteringPoint(aspLine.getMeteringPoint());
-                    line.setParam(t.getParam());
-                    line.setUnit(t.getUnit());
-                    line.setMeter(t.getMeter());
-                    line.setMeterHistory(t.getMeterHistory());
-                    line.setFormula(aspLine.getFormula());
-                    line.setStartMeteringDate(t.getStartMeteringDate());
-                    line.setEndMeteringDate(t.getEndMeteringDate());
-                    line.setStartVal(t.getStartVal());
-                    line.setEndVal(t.getEndVal());
-                    line.setDelta(t.getDelta());
-                    line.setMeterRate(t.getMeterRate());
-                    line.setVal(t.getVal());
-                    line.setBypassMeteringPoint(t.getBypassMeteringPoint());
-                    line.setBypassMode(t.getBypassMode());
-                    line.setUnderCountVal(t.getUnderCountVal());
-                    line.setUndercount(t.getUnderCount());
-                    line.setTreatmentType(aspLine.getTreatmentType());
-                    resultLines.add(line);
-                }
-                saveLines(resultLines);
-            }
-
-            resultLines = new ArrayList<>();
-            for (AspLine aspLine : header.getHeader().getLines()) {
-                if (aspLine.getTreatmentType() != TreatmentTypeEnum.EMPTY)
-                    continue;
-
-                MeteringPoint meteringPoint = aspLine.getMeteringPoint();
-                Parameter param = aspLine.getParam();
-                if (meteringPoint.getMeteringPointTypeId().equals(2l)) {
-                    List<CalcResult> results = calcService.calcMeteringPoints(Arrays.asList(meteringPoint), param.getCode(), context);
-                    Double value = results.size() > 0 ? results.get(0).getDoubleValue() : null;
-
-                    AspResultLine line = new AspResultLine();
-                    line.setHeader(header);
-                    line.setLineNum(aspLine.getLineNum());
-                    line.setMeteringPoint(aspLine.getMeteringPoint());
-                    line.setParam(aspLine.getParam());
-                    line.setUnit(aspLine.getParam().getUnit());
-                    line.setFormula(aspLine.getFormula());
-                    line.setTreatmentType(aspLine.getTreatmentType());
-                    line.setVal(value);
-                    resultLines.add(line);
-                }
-
-                saveLines(resultLines);
-            }
-
-            resultLines = new ArrayList<>();
-            for (AspLine aspLine : header.getHeader().getLines()) {
-                if (aspLine.getTreatmentType() != TreatmentTypeEnum.OUT)
-                    continue;;
-
-                MeteringPoint meteringPoint = aspLine.getMeteringPoint();
-                Parameter param = aspLine.getParam();
-                if (meteringPoint.getMeteringPointTypeId().equals(2l)) {
-                    List<CalcResult> results = calcService.calcMeteringPoints(Arrays.asList(meteringPoint), param.getCode(), context);
-                    Double value = results.size() > 0 ? results.get(0).getDoubleValue() : null;
-
-                    AspResultLine line = new AspResultLine();
-                    line.setHeader(header);
-                    line.setLineNum(aspLine.getLineNum());
-                    line.setMeteringPoint(aspLine.getMeteringPoint());
-                    line.setParam(aspLine.getParam());
-                    line.setUnit(aspLine.getParam().getUnit());
-                    line.setFormula(aspLine.getFormula());
-                    line.setTreatmentType(aspLine.getTreatmentType());
-                    line.setVal(value);
-                    resultLines.add(line);
-                }
-                saveLines(resultLines);
-            }
+            calcInfoRows(header);
+            calcInRows(header, context);
+            calcEmptyRows(header, context);
+            calcOutRows(header, context);
 
             updateStatus(header, BatchStatusEnum.C);
             logger.info("Metering reading for header " + header.getId() + " completed");
@@ -181,6 +84,118 @@ public class AspService {
             logger.error(e.toString() + ": " + e.getMessage());
             e.printStackTrace();
             return false;
+        }
+    }
+
+    private void calcOutRows(AspResultHeader header, CalcContext context) throws Exception {
+        List<AspResultLine> resultLines;
+        resultLines = new ArrayList<>();
+        for (AspLine aspLine : header.getHeader().getLines()) {
+            if (aspLine.getTreatmentType() != TreatmentTypeEnum.OUT)
+                continue;;
+
+            MeteringPoint meteringPoint = aspLine.getMeteringPoint();
+            Parameter param = aspLine.getParam();
+            if (meteringPoint.getMeteringPointTypeId().equals(2l)) {
+                List<CalcResult> results = calcService.calcMeteringPoints(Arrays.asList(meteringPoint), param.getCode(), context);
+                Double value = results.size() > 0 ? results.get(0).getDoubleValue() : null;
+
+                AspResultLine line = new AspResultLine();
+                line.setHeader(header);
+                line.setLineNum(aspLine.getLineNum());
+                line.setMeteringPoint(aspLine.getMeteringPoint());
+                line.setParam(aspLine.getParam());
+                line.setUnit(aspLine.getParam().getUnit());
+                line.setFormula(aspLine.getFormula());
+                line.setTreatmentType(aspLine.getTreatmentType());
+                line.setVal(value);
+                resultLines.add(line);
+            }
+            saveLines(resultLines);
+        }
+    }
+
+    private void calcEmptyRows(AspResultHeader header, CalcContext context) throws Exception {
+        List<AspResultLine> resultLines;
+        resultLines = new ArrayList<>();
+        for (AspLine aspLine : header.getHeader().getLines()) {
+            if (aspLine.getTreatmentType() != TreatmentTypeEnum.EMPTY)
+                continue;
+
+            MeteringPoint meteringPoint = aspLine.getMeteringPoint();
+            Parameter param = aspLine.getParam();
+            if (meteringPoint.getMeteringPointTypeId().equals(2l)) {
+                List<CalcResult> results = calcService.calcMeteringPoints(Arrays.asList(meteringPoint), param.getCode(), context);
+                Double value = results.size() > 0 ? results.get(0).getDoubleValue() : null;
+
+                AspResultLine line = new AspResultLine();
+                line.setHeader(header);
+                line.setLineNum(aspLine.getLineNum());
+                line.setMeteringPoint(aspLine.getMeteringPoint());
+                line.setParam(aspLine.getParam());
+                line.setUnit(aspLine.getParam().getUnit());
+                line.setFormula(aspLine.getFormula());
+                line.setTreatmentType(aspLine.getTreatmentType());
+                line.setVal(value);
+                resultLines.add(line);
+            }
+
+            saveLines(resultLines);
+        }
+    }
+
+    private void calcInRows(AspResultHeader header, CalcContext context) {
+        List<AspResultLine> resultLines;
+        resultLines = new ArrayList<>();
+        for (AspLine aspLine : header.getHeader().getLines()) {
+            if (aspLine.getTreatmentType() != TreatmentTypeEnum.IN)
+                continue;
+
+            List<MeteringReading> meteringReadings = meteringReadingService.calc(aspLine.getMeteringPoint(), context)
+                .stream()
+                .filter(t -> t.getParam().equals(aspLine.getParam()))
+                .collect(Collectors.toList());
+
+            for (MeteringReading t : meteringReadings) {
+                AspResultLine line = new AspResultLine();
+                line.setHeader(header);
+                line.setLineNum(aspLine.getLineNum());
+                line.setMeteringPoint(aspLine.getMeteringPoint());
+                line.setParam(t.getParam());
+                line.setUnit(t.getUnit());
+                line.setMeter(t.getMeter());
+                line.setMeterHistory(t.getMeterHistory());
+                line.setFormula(aspLine.getFormula());
+                line.setStartMeteringDate(t.getStartMeteringDate());
+                line.setEndMeteringDate(t.getEndMeteringDate());
+                line.setStartVal(t.getStartVal());
+                line.setEndVal(t.getEndVal());
+                line.setDelta(t.getDelta());
+                line.setMeterRate(t.getMeterRate());
+                line.setVal(t.getVal());
+                line.setBypassMeteringPoint(t.getBypassMeteringPoint());
+                line.setBypassMode(t.getBypassMode());
+                line.setUnderCountVal(t.getUnderCountVal());
+                line.setUndercount(t.getUnderCount());
+                line.setTreatmentType(aspLine.getTreatmentType());
+                resultLines.add(line);
+            }
+            saveLines(resultLines);
+        }
+    }
+
+    private void calcInfoRows(AspResultHeader header) {
+        List<AspResultLine> resultLines = new ArrayList<>();
+        for (AspLine aspLine : header.getHeader().getLines()) {
+            if (aspLine.getTreatmentType() != TreatmentTypeEnum.INFO)
+                continue;;
+
+            AspResultLine line = new AspResultLine();
+            line.setHeader(header);
+            line.setLineNum(aspLine.getLineNum());
+            line.setTreatmentType(aspLine.getTreatmentType());
+            resultLines.add(line);
+            saveLines(resultLines);
         }
     }
 
