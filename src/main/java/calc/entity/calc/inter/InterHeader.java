59d55288d336d93f2c3bd9b1476fb0d39dcfceeb
eugenenew80 <eugenenew80@gmail.com>
2018-10-18 06:48:43 +0600

    добавил скрипты для создания таблиц для акта свеки МГЛЭП

---
+++ src/main/java/calc/entity/calc/inter/InterHeader.java
@@ -0,0 +1,47 @@
+package calc.entity.calc.inter;
+
+import calc.entity.calc.Organization;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(of= {"id"})
+@Entity
+@Table(name = "calc_act_inter_pl_headers")
+public class InterHeader {
+    @Id
+    private Long id;
+
+    @Column(name = "doc_type_code")
+    private String docTypeCode;
+
+    @ManyToOne
+    @JoinColumn(name = "template_id")
+    private InterHeader template;
+
+    @ManyToOne
+    @JoinColumn(name = "org_id")
+    private Organization organization;
+
+    @Column(name = "state_code")
+    private String stateCode;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    /*
+    @OneToMany(mappedBy = "header", fetch = FetchType.LAZY)
+    private List<AspLine> lines;
+
+    @OneToMany(mappedBy = "header", fetch = FetchType.LAZY)
+    private List<AspNote> notes;
+
+    @OneToMany(mappedBy = "header", fetch = FetchType.LAZY)
+    private List<AspApp> apps;
+    */
+}
---
+++ src/main/java/calc/entity/calc/inter/InterLine.java
@@ -0,0 +1,85 @@
+package calc.entity.calc.inter;
+
+import calc.converter.jpa.BooleanToIntConverter;
+import calc.entity.calc.MeteringPoint;
+import calc.entity.calc.PowerLine;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Immutable;
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(of= {"id"})
+@Entity
+@Table(name = "calc_act_inter_pl_lines")
+@Immutable
+public class InterLine {
+    @Id
+    private Long id;
+
+    @Column(name = "line_num")
+    private Long lineNum;
+
+    @ManyToOne
+    @JoinColumn(name = "header_id")
+    private InterHeader header;
+
+    @ManyToOne
+    @JoinColumn(name = "power_line_id")
+    private PowerLine powerLine;
+
+    @Column(name = "is_bound_meter_inst")
+    @Convert(converter = BooleanToIntConverter.class)
+    private Boolean isBoundMeterInst;
+
+    @Column(name = "is_proportion_length")
+    @Convert(converter = BooleanToIntConverter.class)
+    private Boolean isProportionLength;
+
+    @ManyToOne
+    @JoinColumn(name = "metering_point_id_1")
+    private MeteringPoint meteringPoint1;
+
+    @ManyToOne
+    @JoinColumn(name = "metering_point_id_2")
+    private MeteringPoint meteringPoint2;
+
+    @ManyToOne
+    @JoinColumn(name = "metering_point_id_out_1")
+    private MeteringPoint meteringPointOut1;
+
+    @ManyToOne
+    @JoinColumn(name = "metering_point_id_out_2")
+    private MeteringPoint meteringPointOut2;
+
+    @ManyToOne
+    @JoinColumn(name = "bound_metering_point_id")
+    private MeteringPoint boundMeteringPoint;
+
+    @Column(name = "proportion1")
+    private Double proportion1;
+
+    @Column(name = "proportion2")
+    private Double proportion2;
+
+    @Column(name = "powerLineLength1")
+    private Double powerLineLength1;
+
+    @Column(name = "powerLineLength2")
+    private Double powerLineLength2;
+
+    @Column(name = "powerLineLength")
+    private Double powerLineLength;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    /*
+    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY)
+    private List<AspLineTranslate> translates;
+    */
+}
