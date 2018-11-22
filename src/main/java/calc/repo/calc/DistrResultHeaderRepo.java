59e1cdbc93b9b8536ba268edcbb428ebf296f615
eugenenew80 <eugenenew80@gmail.com>
2018-11-22 08:58:18 +0600

    -

--- src/main/java/calc/repo/calc/DistrResultHeaderRepo.java
+++ src/main/java/calc/repo/calc/DistrResultHeaderRepo.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface DistrResultHeaderRepo extends JpaRepository<DistrResultHeader, Long> {
 
-    @Query("select t from DistrResultHeader t where t.org.id = ?1 and t.dataType = ?2 and t.startDate = ?3 and t.endDate = ?4 and t.isActive ")
+    @Query("select t from DistrResultHeader t where t.org.id = ?1 and t.dataType = ?2 and t.startDate = ?3 and t.endDate = ?4 and t.isActive = true ")
     List<DistrResultHeader> findByOrg(Long orgId, String dataType, LocalDate startDate, LocalDate endDate);
 }
